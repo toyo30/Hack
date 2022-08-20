0b577001d10dc6144c595e10c1da9abd16e63bbc
HaYoonsoo <ahakilo103@gmail.com>
2022-08-21 01:06:04 +0900

    feat: customTime validaet기능 추가

--- src/zustand/useCustomTime.js
+++ src/zustand/useCustomTime.js
@@ -1,46 +1,54 @@
 import create from "zustand";
 
+const validate = ( { date, month, year, second, minute, hour } ) => {
+  //일 변경까지만 구현함. 월과 년도 변경은 없다.
+  const newTime = {
+    date, month, year, second, minute, hour
+  };
+  
+  if ( newTime.second > 59 ) {
+    newTime.second = 0;
+    newTime.minute += 1;
+  }
+  if ( newTime.minute > 59 ) {
+    newTime.minute = 0;
+    newTime.hour += 1;
+  }
+  if ( newTime.hour > 23 ) {
+    newTime.hour = 0;
+    newTime.date += 1;
+  }
+  if ( newTime.second < 0 ) {
+    newTime.second = 59;
+    newTime.minute -= 1;
+  }
+  if ( newTime.minute < 0 ) {
+    newTime.minute = 59;
+    newTime.hour -= 1;
+  }
+  if ( newTime.hour < 0 ) {
+    newTime.hour = 23;
+    newTime.date -= 1;
+  }
+  return newTime;
+}
+
 const useCustomTime = create( set => ( {
   date: 21,
   month: 8,
   year: 2022,
-  second: 0,
-  minute: 0,
-  hour: 1,
+  second: 20,
+  minute: 30,
+  hour: 15,
 
   setDate: (date) => set( () => ({ date: date })),
   setMonth: (month) => set( () => ({ month: month })),
   setYear: (year) => set( () => ({ year: year })),
-  setSecond: (second) => set( () => ({ second: second })),
-  setMinute: (minute) => set( () => ({ minute: minute })),
-  setHour: (hour) => set( () => ({ hour: hour })),
+  setSecond: (second) => set( (state) => validate({ ...state, second: second })),
+  setMinute: (minute) => set( (state) => validate({ ...state, minute: minute })),
+  setHour: (hour) => set( (state) => validate({ ...state, hour: hour })),
 
-  addOneSecond: () => set( (state) => {
-    //일 변경까지만 구현하겠음 월 변경, 년도 변경은 없음
-    let newSecond = state.second;
-    let newMinute = state.minute;
-    let newHour = state.hour;
-    let newDate = state.date;
-    if ( state.second == 59 ) {
-      if ( state.minute == 59 ) {
-        if ( state.hour == 23 ) {
-          newDate += 1;
-        }
-          newHour += 1;          
-      }
-        newMinute += 1;
-    newSecond = 0;
-    } else {
-      newSecond += 1;
-    }
-    return {
-      date: newDate,
-      hour: newHour,
-      minute: newMinute,
-      second: newSecond
-    }
-  } 
-  )
+  addOneSecond: () => set( ( state ) => validate({ ...state, second: state.second + 1 }) ),
 }));
 
 export default useCustomTime;
