1181593c43d6aa449164be4e69965b89a41138e1
lygorani <ksw22662266@gmail.com>
2022-08-20 13:24:35 +0900

    page added

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { authService, dbService } from "fbase";
 import { useNavigate, Link } from "react-router-dom";
+import Page from "../components/Page";
 
 const Signup = ({ isLoggedIn }) => {
     const navigate = useNavigate();
@@ -51,72 +52,74 @@ const Signup = ({ isLoggedIn }) => {
         }
     };
     return (
-        <div>
-            {isLoggedIn ? (
-                <span>이미 가입된 회원이에요</span>
-            ) : (
-                <span>회원가입페이지입니다!</span>
-            )}
-            <form onSubmit={onSubmit}>
-                <input
-                    name="email"
-                    type="text"
-                    placeholder="Email"
-                    required
-                    value={email}
-                    onChange={onChange}
-                />
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    required
-                    value={password}
-                    onChange={onChange}
-                />
-                <input
-                    name="name"
-                    type="text"
-                    placeholder="이름"
-                    required
-                    value={name}
-                    onChange={onChange}
-                />
-                <input
-                    name="wakeTime"
-                    type="text"
-                    placeholder="내일 기상시간"
-                    required
-                    value={wakeTime}
-                    onChange={onChange}
-                />
-                <input
-                    name="sleepTime"
-                    type="text"
-                    placeholder="오늘 잘 시간"
-                    required
-                    value={sleepTime}
-                    onChange={onChange}
-                />
+        <Page>
+            <div>
+                {isLoggedIn ? (
+                    <span>이미 가입된 회원이에요</span>
+                ) : (
+                    <span>회원가입페이지입니다!</span>
+                )}
+                <form onSubmit={onSubmit}>
+                    <input
+                        name="email"
+                        type="text"
+                        placeholder="Email"
+                        required
+                        value={email}
+                        onChange={onChange}
+                    />
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        required
+                        value={password}
+                        onChange={onChange}
+                    />
+                    <input
+                        name="name"
+                        type="text"
+                        placeholder="이름"
+                        required
+                        value={name}
+                        onChange={onChange}
+                    />
+                    <input
+                        name="wakeTime"
+                        type="text"
+                        placeholder="내일 기상시간"
+                        required
+                        value={wakeTime}
+                        onChange={onChange}
+                    />
+                    <input
+                        name="sleepTime"
+                        type="text"
+                        placeholder="오늘 잘 시간"
+                        required
+                        value={sleepTime}
+                        onChange={onChange}
+                    />
 
-                <input type="submit" value="회원가입" />
-            </form>
-            {error === "" ? null : (
-                <div
-                    style={{
-                        marginLeft: "29%",
-                        marginRight: "32%",
-                        color: "red",
-                        marginTop: "0.5%",
-                    }}
-                >
-                    {error}
-                </div>
-            )}
-            <Link to="/signup">회원가입 화면으로 이동하기</Link>
-            <Link to="/login">로그인 화면으로 이동하기</Link>
-            <Link to="/home">홈화면으로 이동하기</Link>
-        </div>
+                    <input type="submit" value="회원가입" />
+                </form>
+                {error === "" ? null : (
+                    <div
+                        style={{
+                            marginLeft: "29%",
+                            marginRight: "32%",
+                            color: "red",
+                            marginTop: "0.5%",
+                        }}
+                    >
+                        {error}
+                    </div>
+                )}
+                <Link to="/signup">회원가입 화면으로 이동하기</Link>
+                <Link to="/login">로그인 화면으로 이동하기</Link>
+                <Link to="/home">홈화면으로 이동하기</Link>
+            </div>
+        </Page>
     );
 };
 
--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Page from "../components/Page";
 
 const Start = () => {
     return (
-        <div>
-            재우미
-            <Link to="/login">로그인 화면으로 이동하기</Link>
-        </div>
+        <Page>
+            <div>
+                재우미
+                <Link to="/login">로그인 화면으로 이동하기</Link>
+            </div>
+        </Page>
     );
 };
 
