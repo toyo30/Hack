d5fc7e63eee58c3a119b81a882b50cec390df35e
HaYoonsoo <ahakilo103@gmail.com>
2022-08-20 15:24:48 +0900

    feat: customTime 개발

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,9 +1,69 @@
 import styled from "styled-components";
+import useCustomTime from "../../zustand/useCustomTime";
+import { useEffect, useState } from "react";
 
 const Page = ({ children }) => {
+  const customTime = useCustomTime();
+
+  const [ isCounterOn, setIsCounterOn ] = useState(true);
+  
+  useEffect( () => {
+    let intervalId;
+    if ( isCounterOn ) {
+      intervalId = setInterval( () => customTime.addOneSecond(), 1000 );
+    }
+    return () => clearInterval(intervalId);
+  }, [isCounterOn])
+
   return (
     <StyledPage>
-      <div>{children}</div>
+      <div className="children">{children}</div>
+      <div className="console">
+        <div>
+          <button onClick={ () => customTime.setYear( customTime.year - 1 )}>-</button>
+            <span>
+              year: {customTime.year}
+            </span>
+          <button onClick={ () => customTime.setYear( customTime.year + 1 )}>+</button>
+        </div>
+        <div>
+          <button onClick={ () => customTime.setMonth( customTime.month - 1 )}>-</button>
+            <span>
+              month: {customTime.month}
+            </span>
+          <button onClick={ () => customTime.setMonth( customTime.month + 1 )}>+</button>
+        </div>
+        <div>
+          <button onClick={ () => customTime.setDate( customTime.date - 1 )}>-</button>
+            <span>
+              date: {customTime.date}
+            </span>
+          <button onClick={ () => customTime.setDate( customTime.date + 1 )}>+</button>
+        </div>
+        <div>
+          <button onClick={ () => customTime.setHour( customTime.hour - 1 )}>-</button>
+            <span>
+              hour: {customTime.hour}
+            </span>
+          <button onClick={ () => customTime.setHour( customTime.hour + 1 )}>+</button>
+        </div>
+        <div>
+          <button onClick={ () => customTime.setMinute( customTime.minute - 1 )}>-</button>
+            <span>
+              minute: {customTime.minute}
+            </span>
+          <button onClick={ () => customTime.setMinute( customTime.minute + 1 )}>+</button>
+        </div>
+        <div>
+          <button onClick={ () => customTime.setSecond( customTime.second - 1 )}>-</button>
+            <span>
+              second: {customTime.second}
+            </span>
+          <button onClick={ () => customTime.setSecond( customTime.second + 1 )}>+</button>
+        </div>
+        <button onClick={() => setIsCounterOn( (prev) => !prev)}>자동</button>
+        
+      </div>
     </StyledPage>
   );
 };
@@ -13,13 +73,33 @@ const StyledPage = styled.div`
   width: 100%;
   min-height: 100vh;
   background-color: rgb(160, 160, 160);
-  display: flex;
-  justify-content: center;
-  > div {
-    position: relative;
+  > div.children {
+    position: absolute;
+    left: 0px;
+    right: 0px;
+    margin-left: auto;
+    margin-right: auto;
     width: 360px;
     height: 640px;
     background-color: white;
+  }
+
+  > div.console {
+    position: absolute;
+    background-color: white;
+    top: 100px;
+    left: 100px;
+    width: max-content;
+    height: max-content;
+
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    > div {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
   }
 `;
 
---
+++ src/zustand/useCustomTime.js
@@ -0,0 +1,46 @@
+import create from "zustand";
+
+const useStore = create( set => ( {
+  date: 21,
+  month: 8,
+  year: 2022,
+  second: 0,
+  minute: 0,
+  hour: 1,
+
+  setDate: (date) => set( () => ({ date: date })),
+  setMonth: (month) => set( () => ({ month: month })),
+  setYear: (year) => set( () => ({ year: year })),
+  setSecond: (second) => set( () => ({ second: second })),
+  setMinute: (minute) => set( () => ({ minute: minute })),
+  setHour: (hour) => set( () => ({ hour: hour })),
+
+  addOneSecond: () => set( (state) => {
+    //일 변경까지만 구현하겠음 월 변경, 년도 변경은 없음
+    let newSecond = state.second;
+    let newMinute = state.minute;
+    let newHour = state.hour;
+    let newDate = state.date;
+    if ( state.second == 59 ) {
+      if ( state.minute == 59 ) {
+        if ( state.hour == 23 ) {
+          newDate += 1;
+        }
+          newHour += 1;          
+      }
+        newMinute += 1;
+    newSecond = 0;
+    } else {
+      newSecond += 1;
+    }
+    return {
+      date: newDate,
+      hour: newHour,
+      minute: newMinute,
+      second: newSecond
+    }
+  } 
+  )
+}));
+
+export default useStore;
