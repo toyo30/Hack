014e7a39d17c14b54438d2ae8641c2e543786870
Kim NaYoung <99931269+nymvmt@users.noreply.github.com>
2022-08-20 22:23:17 +0900

    ds

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,6 @@ import Link from "../components/Link";
 import Page from "../components/Page";
 import Text from "../components/Text";
 import Button from "../components/Button";
-import Logo from "../components/Logo";
 import NavBar from "components/NavBar";
 
 
@@ -98,7 +97,7 @@ const Home = ({ isLoggedIn, userObj }) => {
                 </Text>
             ) : (
                 <div>
-                    <Text>홈화면입니다. 지금 로그인되지 않았어요</Text>
+                    <Text>지금 로그인되지 않았어요</Text>
                     <Link to="/signup"><Button>회원가입</Button></Link>
                     <Link to="/login"><Button>로그인</Button></Link>
                 </div>
@@ -118,6 +117,14 @@ const Home = ({ isLoggedIn, userObj }) => {
                 ) : null} */}
             {sleepInfoInit ? (
                 <div>
+                  <button
+                onClick={() => {
+                    authService.signOut();
+                    navigate("/first");
+                }}
+            >
+                로그아웃
+            </button>
                     <div>내일 기상시간 :{wakeTime}</div>
                     <div>수면시간 : {sleepTime}</div>
                     {sleepStartTime ? (
