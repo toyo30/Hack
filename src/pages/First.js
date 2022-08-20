149b8e973f2727b2be704e3cf4f9d830da0c67af
nymvmt <tkfkd9008a@naver.com>
2022-08-20 13:16:19 +0900

    로그아웃, 윤수 풀받기

---
+++ src/pages/First.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+
+function First() {
+    return (
+    <>
+        <img src=""></img>
+        <Box>
+          <h1>재우미</h1>
+        </Box>
+    </>);
+};
+
+export default First;
+
+const Box = styled.div`
+    background:#232226;
+`;
+
+const h1 = styled.div`
+    color: #FFFFFF;
+`;
