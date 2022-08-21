446659986ba3baff293ec5ed7932cee6bd8cfe28
HaYoonsoo <ahakilo103@gmail.com>
2022-08-21 13:57:10 +0900

    fix: Link component color issue

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const Link = styled(ReactLink)`
   text-decoration: none;
+  color: inherit;
 
   &:focus, &:hover, &:visited, &:link, &:active {
     text-decoration: none;
